a19526c91b59d482ce287873d62d353c8cc4e60d
Jose Augusto Camacho Fernandez <jcamachofernandez@gmail.com>
2015-11-17 19:30:50 +0100

    3  Actividad  "Intents " ------------commit

---
+++ app/src/main/java/es/carlostessier/mispruebas/Intents_Act_01_b.java
@@ -0,0 +1,15 @@
+package es.carlostessier.mispruebas;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Intents_Act_01_b extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intents__act_01_b);
+
+    }
+
+}
